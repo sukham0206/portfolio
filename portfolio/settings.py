79de69cbbfcb044a151bb4dcf68d30807524cd3c
Sukham <schatha_be20@thapar.edu>
2022-07-02 15:08:43 +0530

    commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -125,6 +125,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
     ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+    ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
